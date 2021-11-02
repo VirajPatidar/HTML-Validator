8d934a7c7492eae5fdfcb918f14bd5cd4a5369fb
Viraj Patidar <vrj381@gmail.com>
2021-11-02 12:34:52 +0530

    extracted all tags into a vector

--- validator.cpp
+++ validator.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<string> tags;
+void addTagToList(string);
+
 int main() {
     #ifndef INPUT_OUTPUT
         freopen("input.txt", "r", stdin);
@@ -10,6 +13,23 @@ int main() {
     while(!feof(stdin)) {
         string s;
         getline(cin, s);
+        addTagToList(s);
         cout << s << endl;
+    }
+
+    for (auto i : tags)
+    {
+        cout << i << endl;
+    }
+    
+}
+
+void addTagToList(string s){
+    const regex tagSyntax("\\<.*?\\>");
+    smatch match;
+    if (regex_search(s, match, tagSyntax)){
+        for (int i = 0 ; i < match.size(); ++i){
+            tags.push_back(match[i]);
+        }
     }
 }
