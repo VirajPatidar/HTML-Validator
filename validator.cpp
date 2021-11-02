7294ee0b01beb130c1e6e719345b20152bf207b2
Viraj Patidar <vrj381@gmail.com>
2021-11-02 11:08:17 +0530

    input ouput using file handling

--- validator.cpp
+++ validator.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main() {
     #ifndef INPUT_OUTPUT
         freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdin);
+        freopen("output.txt", "w", stdout);
     #endif
+    int c =0;
+    while(!feof(stdin)) {
+        string s;
+        getline(cin, s);
+        cout << s << endl;
+    }
 }
