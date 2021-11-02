cba2731518fc0bf15ec5b6b04ad6205959e41827
Viraj Patidar <vrj381@gmail.com>
2021-11-02 10:14:50 +0530

    Initial Setup

---
+++ validator.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    #ifndef INPUT_OUTPUT
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdin);
+    #endif
+}
