4c5fd2963fa35a93e8ec4c2e749dea73b96dfa2c
Viraj Patidar <vrj381@gmail.com>
2021-11-02 14:45:42 +0530

    checking tag syntax

--- validator.cpp
+++ validator.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 vector<string> tags;
-void addTagToList(string);
+int addTagToList(string);
 void printTagList();
 
 int main() {
@@ -14,25 +14,58 @@ int main() {
     while(!feof(stdin)) {
         string s;
         getline(cin, s);
-        addTagToList(s);
+        if(addTagToList(s) == 1) {
+            cout << s << endl;
+            cout << "^Error: Invalid HTML tag syntax caught above";
+            exit(0);
+        }
         cout << s << endl;
     }
 
     printTagList();
 }
 
-void addTagToList(string s){
+bool isValidHTMLTag(string str) {
+    // Function is not checking all cases
+    // Regex to check valid HTML tag.
+    const regex pattern("<(\"[^\"]*\"|'[^']*'|[^'\">])*>");
+ 
+    // If the HTML tag
+    // is empty return false
+    if (str.empty()) {
+        return false;
+    }
+    
+    // Return true if the HTML tag
+    // matched the ReGex
+    if(regex_match(str, pattern)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int addTagToList(string s){
     const regex tagSyntax("\\<.*?\\>");
 
+    int tagCount = 0;
     sregex_iterator iter(s.begin(), s.end(), tagSyntax);
     sregex_iterator end;
 
     while(iter != end) {
         for(unsigned i = 0; i < iter->size(); ++i) {
-            tags.push_back((*iter)[i]);
+            if(isValidHTMLTag((*iter)[i])) {
+                tags.push_back((*iter)[i]);
+                tagCount++;
+            }
+            else{
+                return 1;
+            }
         }
         ++iter;
     }
+    return 0;
 }
 
 void printTagList(){
