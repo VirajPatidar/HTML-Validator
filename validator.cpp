8ea2c77f7aea8405b578919dc69012e1afb4a7ec
VedantNakrani <vedant2301nakrani@gmail.com>
2021-11-03 17:14:35 +0530

    checkOrder() completed

--- validator.cpp
+++ validator.cpp
@@ -4,6 +4,7 @@ using namespace std;
 vector<string> tags;
 int addTagToList(string);
 void printTagList();
+int checkOrder();
 
 int main() {
     #ifndef INPUT_OUTPUT
@@ -23,6 +24,11 @@ int main() {
     }
 
     printTagList();
+
+    if(checkOrder() == 1){
+        cout << "^Error: Invalid HTML tag syntax caught above";
+        exit(0);
+    }
 }
 
 bool isValidHTMLTag(string str) {
@@ -46,6 +52,55 @@ bool isValidHTMLTag(string str) {
     }
 }
 
+int checkOrder() {
+    
+    if(tags[0] != "<!DOCTYPE html>") {
+        cout << "^Error: '<!DOCTYPE html>' tag not included.";
+        return 1;
+    }
+    if(tags[1].find("html") == 4294967295) {
+        cout << "^Error: '<html>' tag not initialized.";
+        return 1;
+    }
+    int flag1 = -1;
+    int flag2 = 0;
+    char * head_tags[2] = {"meta", "title"};
+    
+    for(auto i : tags){
+        
+        flag2 = 0;
+        if(i == "<head>") {
+            flag1 = 0;
+            continue;
+        }
+        if(i == "</head>") {
+            flag1 = 1;
+            continue;
+        }
+
+        if(flag1 == -1 && i == "<body>") {
+            cout << "^Error: <body> tag must come after </head> tag.";
+            return 1;
+        }
+
+        if(flag1 == 0) {
+            for(auto j : head_tags) {
+                cout << i << ", " << j << ", ";
+                cout << i.find(j) << endl;
+                if(i.find(j) != 4294967295) {
+                    flag2 = 1;
+                    break;
+                }
+            }
+            if(flag2 == 0) {
+                cout << "^Error: "<< i <<" tag must not come inside <head> tag.";
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
 int addTagToList(string s){
     const regex tagSyntax("\\<.*?\\>");
 
@@ -65,6 +120,7 @@ int addTagToList(string s){
         }
         ++iter;
     }
+
     return 0;
 }
 
@@ -75,4 +131,6 @@ void printTagList(){
     for(auto i : tags){
         cout << i << endl;
     }
+    cout << "------------------------------------------\n";
+    cout << "------------------------------------------\n";
 }
